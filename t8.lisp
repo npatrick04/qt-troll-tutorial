7054c3e05616f0077a412950bd2e86af28cf4314
Nick Patrick <npatrick04@gmail.com>
2015-01-25 21:20:56 -0600

    Update to T10.

--- t8.lisp
+++ t8.lisp
@@ -54,11 +54,16 @@
 (defclass cannon-field ()
   ((current-angle :accessor current-angle
                   :initform 45
-                  :initarg :current-angle))
+                  :initarg :current-angle)
+   (current-force :accessor current-force
+                  :initform 0
+                  :initarg :current-force))
   (:metaclass qt-class)
   (:qt-superclass "QWidget")
-  (:slots ("setAngle(int)" set-angle))
-  (:signals ("angleChanged(int)"))
+  (:slots ("setAngle(int)" set-angle)
+          ("setForce(int)" set-force))
+  (:signals ("angleChanged(int)")
+            ("forceChanged(int)"))
   (:override ("paintEvent" paint-event)))
 
 (defmethod initialize-instance :after ((instance cannon-field) &key parent)
@@ -79,8 +84,15 @@
   (let ((angle (clamp angle 5 70)))
     (unless (= (current-angle cannon) angle)
       (setf (current-angle cannon) angle)
-      (#_update cannon)
+      (#_update cannon (cannon-rect cannon))
       (emit-signal cannon "angleChanged(int)" angle))))
+
+(defmethod set-force ((cannon cannon-field) force)
+  (declare (integer force))
+  (let ((force (clamp force 10 50)))
+    (unless (= (current-force cannon) force)
+      (setf (current-force cannon) force)
+      (emit-signal cannon "forceChanged(int)" force))))
 
 (defmethod paint-event ((cannon cannon-field) event)
   (let ((painter (#_new QPainter cannon)))
@@ -92,7 +104,7 @@
     (#_setPen painter (#_NoPen "Qt"))
     (#_setBrush painter (#_blue "Qt"))
     
-    (#_translate painter 0 (#_height (#_rect cannon)))
+    (#_translate painter 0 (#_height cannon))
     (with-objects ((pie    (#_new QRect -35 -35 70 70))
                    (barrel (#_new QRect 30 -5 20 10)))
       (#_drawPie painter pie 0 (* 90 16))
@@ -100,6 +112,11 @@
       (#_drawRect painter barrel))
     
     (#_end painter)))
+
+(defmethod cannon-rect ((cannon cannon-field))
+  (let ((result (#_new QRect 0 0 50 50)))
+    (#_moveBottomLeft result (#_bottomLeft (#_rect cannon)))
+    result))
 
 ;;; Main window
 
@@ -117,28 +134,39 @@
   (let ((quit   (#_new QPushButton "&Quit" object))
         (font   (#_new QFont "Times" 18 (#_Bold "QFont")))
         (angle  (make-instance 'lcd-range))
+        (force  (make-instance 'lcd-range))
         (cannon-field (make-instance 'cannon-field))
-        (grid   (#_new QGridLayout)))
+        (left-layout (#_new QVBoxLayout))
+        (grid        (#_new QGridLayout)))
 
     (#_setFont quit font)
     (connect quit "clicked()" *qapplication* "quit()")
 
     (set-range angle 5 70)
+    (set-range force 10 50)
     
     (connect angle "valueChanged(int)" cannon-field "setAngle(int)")
     (connect cannon-field "angleChanged(int)" angle "setValue(int)")
     
+    (connect force "valueChanged(int)" cannon-field "setForce(int)")
+    (connect cannon-field "forceChanged(int)" force "setValue(int)")
+
+    (#_addWidget left-layout angle)
+    (#_addWidget left-layout force)
+    
     (#_addWidget grid quit 0 0)
-    (#_addWidget grid angle 1 0)
+    (#_addLayout grid left-layout 1 0)
     (#_addWidget grid cannon-field 1 1 2 1)
     (#_setColumnStretch grid 1 10)
 
     (#_setLayout object grid)
     
     (set-value angle 60)
+    (set-value force 25)
     (#_setFocus angle)))
 
 (defun main ()
   "http://doc.qt.digia.com/4.3/tutorial-t8.html"
   (with-main-window
-      (window (make-instance 'widget))))
+      (window (make-instance 'widget))
+    (#_setGeometry window 100 100 500 355)))
